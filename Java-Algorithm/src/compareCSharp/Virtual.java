793cafa2c072d1d968f492ef8116b80735eefaed
Arthur Liu <arthur.liu@cgic.cooperators.ca>
2019-06-13 15:15:17 -0400

    compare with c# for Virtural method

---
+++ Java-Algorithm/src/compareCSharp/Virtual.java
@@ -0,0 +1,31 @@
+package compareCSharp;
+
+abstract class Dog {
+    final void bark() {               //bark() is not virtual because it is
+        System.out.println("woof");   //final and if you tried to override it
+    }                                 //you would get a compile time error.
+
+
+     void bark2() {
+        System.out.println("woof2");
+    }
+
+    abstract void jump();             //jump() is a virtual function because it
+}                                     //is part of an abstract class and isn't
+//final.
+class MyDog extends Dog{
+    void jump(){
+        System.out.println("boing");    //here jump() is being overridden, a
+    }                                   //demonstration that it is virtual.
+
+    void bark2() {
+        System.out.println("woof2b");
+    }
+}
+public class Virtual  {
+    public static void main(String[] args) {
+        MyDog myDog = new MyDog();       //instantiating myDog
+       // myDog.jump();                    //calling the overridden function jump()
+        myDog.bark2();
+    }
+}
